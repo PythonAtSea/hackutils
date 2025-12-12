cf4aff53c9bebf089390fd6c336ded610d85c5ef
PythonAtSea <HudsonGreen27@gmail.com>
2025-12-12 17:48:17 -0500

    fix: move copy button inside the UUID box

--- src/app/uuid/page.tsx
+++ src/app/uuid/page.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 export default function Page() {
   const [uuid, setUuid] = useState("");
-  const [copyIcon, setCopyIcon] = useState(<Copy size={16} />);
+  const [copyIcon, setCopyIcon] = useState(<Copy size={16} className="ml-3" />);
 
   useEffect(() => {
     setUuid(crypto.randomUUID());
@@ -14,19 +14,25 @@ export default function Page() {
     <div className="flex flex-row items-center justify-center py-2 size-full gap-4">
       {uuid && (
         <>
-          <div className="rounded-md h-9 border bg-background text-center justify-center flex flex-col items-center px-3 py-2 select-all shadow-xs dark:border-input dark:bg-border/30">
-            {uuid}
-          </div>
-          <Button
-            variant="outline"
+          <div
+            className="rounded-md h-9 border bg-background text-center justify-center flex flex-row items-center px-3 py-2 select-none shadow-xs dark:border-input dark:bg-border/30 cursor-pointer hover:bg-accent dark:hover:bg-input/50 transition-all"
             onClick={() => {
               navigator.clipboard.writeText(uuid);
-              setCopyIcon(<Check size={16} />);
-              setTimeout(() => setCopyIcon(<Copy size={16} />), 1000);
+              setCopyIcon(
+                <Check
+                  size={16}
+                  className="ml-3 text-green-600 dark:text-green-400"
+                />
+              );
+              setTimeout(
+                () => setCopyIcon(<Copy size={16} className="ml-3" />),
+                2000
+              );
             }}
           >
+            {uuid}
             {copyIcon}
-          </Button>
+          </div>
         </>
       )}
       <Button onClick={() => setUuid(crypto.randomUUID())}>re-roll</Button>
